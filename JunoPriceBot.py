0ee495fdfedde19cdb064a641ad9e2deb0d02b0e
Camel <98026097+CamelJuno@users.noreply.github.com>
2022-07-29 11:59:06 +0200

    fetch price data off osmo and remove flask

--- JunoPriceBot.py
+++ JunoPriceBot.py
@@ -1,52 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 
-#Coded by CamelJuno 🐪#7465
-
 import discord
 import requests
 import json
 import time
-from flask import Flask
-from threading import Thread 
-
-app = Flask('')
-
-@app.route('/')
-def home():
-  return 'Camel is here'
-
-def run():
-  app.run(host='0.0.0.0',port=8000)
-
-def keep_alive():
-  t = Thread(target=run)
-  t.start()
 
 client = discord.Client()
-def getJUNO2UST():
+def getJUNOOsmosis():
   headers = {
-    'Host': 'api.coingecko.com',
-    'Accept': '*/*',
+    'Host': 'api-osmosis.imperator.co',
+    'Accept': '*/*'
   }
-  a = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=juno-network',headers=headers,timeout=10)
+  a = requests.get('https://api-osmosis.imperator.co/tokens/v2/JUNO',headers=headers,timeout=20)
   if a.status_code == 200:
-    z = json.loads(a.text)[0]['current_price']
-    b = json.loads(a.text)[0]['market_cap']
-    return z,b
+    z = round(json.loads(a.text)[0]['price'],2)
+    y = round(json.loads(a.text)[0]['price_24h_change'],2)
+    if z == None:
+      getJUNOOsmosis()
+    else:
+      return z,y
   else:
-    getJUNO2UST()
-
-def formatIt(hello):
-  suffixes = ["", "", "M", "B", "T"]
-  hello = str("{:,}".format(hello))
-  commas = 0
-  x = 0
-  while x < len(hello):
-      if hello[x] == ',':
-          commas += 1
-      x += 1
-  return hello.split(',')[0]+'.'+hello.split(',')[1][:-1] + suffixes[commas]
+    getJUNOOsmosis()
 
 @client.event
 async def on_ready():
@@ -55,17 +30,15 @@ async def on_ready():
   member = guild.get_member(memberID)
   while(True):
     try:
-      JUNO2UST,JunoMC = getJUNO2UST()
-      print(JUNO2UST)
-      print(JunoMC)
-      await member.edit(nick="JUNO $"+str(JUNO2UST))
+      price,PriceChange = getJUNOOsmosis()
+      await member.edit(nick="JUNO $"+str(price))
       time.sleep(1)
-      await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="MC: $"+str(formatIt(JunoMC))))
-      time.sleep(30)
+      if PriceChange > 0:
+        PriceChange = '+'+str(PriceChange)
+      await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Δ 24h: "+str(PriceChange)+"%"))
+      time.sleep(28)
     except:
       continue
-
-keep_alive()
 
 guildID = 0
 memberID = 0
